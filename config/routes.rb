9b31737af686f4f07e7c4ab84aa70c55273073af
Fernando Gouveia <gouveiaramos.me@gmail.com>
2018-10-30 12:43:23 +0000

    lsti

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'soma/ex1/num1/:n1/num2/:2' => "soma#ex1"
-  get 'teste/page'
+  #get 'soma/ex1/num1/:n1/num2/:2' => "soma#ex1"--
+  #get 'teste/page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#index"
-  get "/teste" => "application#teste"
+
+  #get "/teste" => "application#teste"
+  
 end
